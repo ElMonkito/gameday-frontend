c3ed469f08d69631b6ee11d29f869b86bd918191
ElMonkito <rh@RT-QLS-NBK-022.ad.reist-tele.com>
2025-03-05 14:47:27 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Fetch from './Fetch.jsx';
+
+const App = () => {
+    return(
+        <div>
+            <h1>Fetch Data from Flask</h1>
+            <Fetch />
+        </div>
+    )
+}
+
+export default App;

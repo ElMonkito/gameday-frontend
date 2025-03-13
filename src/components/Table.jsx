c696405be80d38b461d704575c4406106f21117f
ElMonkito <rh@RT-QLS-NBK-022.ad.reist-tele.com>
2025-03-13 10:36:10 +0100

    loader of data

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -7,12 +7,14 @@ import TableRow from "@mui/material/TableRow";
 import TableCell from "@mui/material/TableCell";
 import TableBody from "@mui/material/TableBody";
 import Table from "@mui/material/Table";
+import CircularProgress from "@mui/material/CircularProgress";
 import LigueFilter from "./LigueFilter";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import RegionFilter from "./RegionFilter";
 
 export default function TableGame() {
     const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     // Ligue
     const [selectedColumnLigue, setSelectedColumnLigue] = useState("");
@@ -25,6 +27,7 @@ export default function TableGame() {
     const [selectedValueRegion, setSelectedValueRegion] = useState("");
 
     useEffect(() => {
+        setIsLoading(true); // Activer l'état de chargement
         axios.get(process.env.REACT_APP_BACKEND_URL)
             .then(response => {
                 setData(response.data);
@@ -42,7 +45,8 @@ export default function TableGame() {
                     setColumnValuesRegion([...new Set(response.data.map(row => row[columnKeyRegion]))]);
                 }
             })
-            .catch(error => console.error("Erreur :", error));
+            .catch(error => console.error("Erreur :", error))
+            .finally(() => setIsLoading(false));
     }, []);
 
     return (
@@ -61,36 +65,45 @@ export default function TableGame() {
                 columnName={selectedColumnRegion}
             />
 
-            <TableContainer component={Paper} sx={{ maxWidth: "90%", margin: "20px auto", borderRadius: "10px", boxShadow: 3 }}>
-                <Table sx={{ minWidth: 650 }} aria-label="tableau des matchs">
-                    <TableHead>
-                        <TableRow sx={{ backgroundColor: "#41644A" }}>
-                            {data.length > 0 && Object.keys(data[0]).map((key) => (
-                                <TableCell key={key} sx={{ color: "white", fontWeight: "bold", textTransform: "uppercase" }}>
-                                    {key}
-                                </TableCell>
-                            ))}
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {data
-                            .filter(row => selectedValueLigue === "" || row[selectedColumnLigue] === selectedValueLigue)
-                            .filter(row => selectedValueRegion === "" || row[selectedColumnRegion] === selectedValueRegion)
-                            .map((row, index) => (
-                                <TableRow key={index} sx={{
-                                    backgroundColor: index % 2 === 0 ? "#f5f5f5" : "white",
-                                    '&:hover': { backgroundColor: "#9aada0" }
-                                }}>
-                                    {Object.values(row).map((val, i) => (
-                                        <TableCell key={i} sx={{ padding: "10px" }}>
-                                            {val || "-"}
-                                        </TableCell>
-                                    ))}
-                                </TableRow>
-                            ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+            {isLoading ? (
+                <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" height="50vh">
+                    <CircularProgress size={80} sx={{ color: "#41644A"}} />
+                    <Typography variant="h6" mt={2} color="gray">
+                        Chargement ...
+                    </Typography>
+                </Box>
+            ) : (
+                <TableContainer component={Paper} sx={{ maxWidth: "90%", margin: "20px auto", borderRadius: "10px", boxShadow: 3 }}>
+                    <Table sx={{ minWidth: 650 }} aria-label="tableau des matchs">
+                        <TableHead>
+                            <TableRow sx={{ backgroundColor: "#41644A" }}>
+                                {data.length > 0 && Object.keys(data[0]).map((key) => (
+                                    <TableCell key={key} sx={{ color: "white", fontWeight: "bold", textTransform: "uppercase" }}>
+                                        {key}
+                                    </TableCell>
+                                ))}
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {data
+                                .filter(row => selectedValueLigue === "" || row[selectedColumnLigue] === selectedValueLigue)
+                                .filter(row => selectedValueRegion === "" || row[selectedColumnRegion] === selectedValueRegion)
+                                .map((row, index) => (
+                                    <TableRow key={index} sx={{
+                                        backgroundColor: index % 2 === 0 ? "#f5f5f5" : "white",
+                                        '&:hover': { backgroundColor: "#9aada0" }
+                                    }}>
+                                        {Object.values(row).map((val, i) => (
+                                            <TableCell key={i} sx={{ padding: "10px" }}>
+                                                {val || "-"}
+                                            </TableCell>
+                                        ))}
+                                    </TableRow>
+                                ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            )}
         </Box>
     );
 }
